61ae9ff28dfa83e56f3a42d0d1e7f4a08c79d88c
JesusBruque <bruke3_voleyutrera@hotmail.com>
2022-02-05 10:46:16 +0000

    commit

--- src/models.py
+++ src/models.py
@@ -8,26 +8,46 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    password = Column(String(20), nullable=False)
+    nickname = Column(String(100))
 
-class Address(Base):
-    __tablename__ = 'address'
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250))
+    birth_year = Column(String(250))
+    gender = Column(String(250))
+    height = Column(Integer)
+    skin_color = Column(String(250))
+    eye_color = Column(String(250))
 
-    def to_dict(self):
-        return {}
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    climate = Column(String(250))
+
+class UserFavourite(Base):
+    __tablename__ = 'user_favourite'
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    id_character = Column(Integer, ForeignKey('character.id'))
+    id_planet = Column(Integer, ForeignKey('planet.id'))
+    relationship(User)
+    relationship(Character)
+    relationship(Planet)
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
